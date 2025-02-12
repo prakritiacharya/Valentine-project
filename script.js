58a874e9c5a531abcd93c2f3d505629c2788b995
Prakriti Acharya <prakriti@acharya.com>
2025-02-11 22:22:42 -0500

    added animation on yes button

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const messages = [
     "Are you positive?",
     "Pookie please...",
     "Just think about it!",
-    "If you say no, I will be really sad...",
+    "If you say no, I will be sad...",
     "I will be very sad...",
     "I will be very very very sad...",
     "Ok fine, I will stop asking...",
